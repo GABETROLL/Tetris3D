e13019f1e1353cb3c0895ecf05e3f673566f9bad
GABETROLL <gabriel7icplhappy@gmail.com>
2023-08-20 22:24:16 -0400

    Add modern score class

--- game/score.py
+++ game/score.py
@@ -1,6 +1,60 @@
 from dataclasses import dataclass
 
 
+NO_T_SPIN = 0
+MINI_T_SPIN = 1
+T_SPIN = 2
+
+T_SPIN_TYPES = (NO_T_SPIN, MINI_T_SPIN, T_SPIN)
+
+
+@dataclass
+class ModernScore:
+    points: int = 0
+    lines: int = 0
+    level: int = 0
+    back_to_back: bool = False
+    combo_count: int = 0
+
+    def score_soft_drop(self, blocks_traveled: int):
+        self.points += blocks_traveled
+
+    def score_hard_drop(self, blocks_traveled: int):
+        self.points += (blocks_traveled << 1)
+
+    def score_clear(self, cleared_lines: int, t_spin_type: int, was_all_clear: bool = False) -> None:
+        just_line_clear_scores = 0, 100, 300, 500, 800
+
+        points_gained = self.level + 1
+
+        # t-spin / mini t-spin / normal
+        if t_spin_type == T_SPIN:
+            points_gained *= (400 + 400 * cleared_lines)
+        elif t_spin_type == MINI_T_SPIN:
+            points_gained *= 100 * (1 << cleared_lines)
+        else:
+            points_gained *= just_line_clear_scores[cleared_lines]
+
+        # all clear
+        if was_all_clear:
+            pass
+
+        # back-to-back
+        if self.back_to_back:
+            points_gained *= 1.5
+        if cleared_lines >= 4 or (t_spin_type == MINI_T_SPIN and cleared_lines) or (t_spin_type == T_SPIN and cleared_lines):
+            self.back_to_back = True
+        else:
+            self.back_to_back = False
+
+        # combo
+        points_gained += 50 * self.combo_count * self.level
+        if cleared_lines:
+            self.combo_count += 1
+        else:
+            self.combo_count = 0
+
+
 @dataclass
 class Score:
     points: int = 0
